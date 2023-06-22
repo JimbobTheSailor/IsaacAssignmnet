dcdac4dc9fd7a5a8003838ee16de6b19d59e49de
James Whitehall <James.Whitehall@outlook.com>
2023-06-22 21:44:01 +1000

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;   
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MathGame MyMathGame { get; set; }
+        public string MyQuestion { get; set; } = "Works";
+        public MainWindow()
+        {
+            MyMathGame = new MathGame();
+            InitializeComponent();
+            SliderRounds.Value = MyMathGame.GameRounds;
+            HideAllControls();
+        }
+
+        private void Hard_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            if (cb.IsChecked.HasValue)
+            {
+                if (cb.IsChecked.Value == true)
+                {
+                    if(DifficultyCheckBoxMedium!=null)
+                        DifficultyCheckBoxMedium.IsChecked = false;
+                    if(DifficultyCheckBoxEasy!=null)
+                        DifficultyCheckBoxEasy.IsChecked = false;
+                    MyMathGame.SetDifficultly(true, false, false);
+                }
+            }
+        }
+        private void Med_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+
+            if (cb.IsChecked.HasValue)
+            {
+                if (cb.IsChecked.Value == true)
+                {
+                    if(DifficultyCheckBoxHard!=null)
+                        DifficultyCheckBoxHard.IsChecked = false;
+
+                    if(DifficultyCheckBoxEasy!=null)
+                        DifficultyCheckBoxEasy.IsChecked = false;
+                    MyMathGame.SetDifficultly(false, true, false);
+                }
+              
+            }
+        }
+        private void Easy_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb == null)
+                return;
+            if (cb.IsChecked.HasValue)
+            {
+                if (cb.IsChecked.Value == true)
+                {
+                    if(DifficultyCheckBoxMedium!=null)
+                        DifficultyCheckBoxMedium.IsChecked = false;
+                    if (DifficultyCheckBoxHard != null)
+                        DifficultyCheckBoxHard.IsChecked = false;
+                    MyMathGame.SetDifficultly(false, false, true);
+                }
+               
+            }
+        }
+        private void SelectAll_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxAdd.IsChecked = CheckBoxMult.IsChecked = CheckBoxSub.IsChecked = CheckBoxDiv.IsChecked = true;
+        }
+
+        private void SelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBoxAdd.IsChecked = CheckBoxMult.IsChecked = CheckBoxSub.IsChecked = CheckBoxDiv.IsChecked = false;
+        }
+
+        private void SelectAll_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            // If the SelectAll box is checked (all options are selected), 
+            // clicking the box will change it to its indeterminate state.
+            // Instead, we want to uncheck all the boxes,
+            // so we do this programatically. The indeterminate state should
+            // only be set programatically, not by the user.
+
+            if (CheckBoxAdd.IsChecked == true &&  CheckBoxMult.IsChecked == true &&   CheckBoxSub.IsChecked == true &&  CheckBoxDiv.IsChecked == true)
+            {
+                // This will cause SelectAll_Unchecked to be executed, so
+                // we don't need to uncheck the other boxes here.
+                OptionsAllCheckBox.IsChecked = false;
+            }
+        }
+
+        private void HideAllControls()
+        {
+            Questions.Visibility = Visibility.Hidden;
+            UserInput.Visibility = Visibility.Hidden;
+           // ButtonAns.Visibility = Visibility.Hidden;
+            TextBlockAnswer.Visibility = Visibility.Hidden;
+            TextBlockGameOver.Visibility = Visibility.Hidden;
+        }
+        private void SetCheckedState()
+        {
+            // Controls are null the first time this is called, so we just 
+            // need to perform a null check on any one of the controls.
+            if (CheckBoxDiv != null)
+            {
+                if (CheckBoxAdd.IsChecked == true &&  CheckBoxMult.IsChecked == true &&  CheckBoxSub.IsChecked == true && CheckBoxDiv.IsChecked == true)
+                {
+                    OptionsAllCheckBox.IsChecked = true;
+                }
+                else if (CheckBoxAdd.IsChecked == false &&  CheckBoxMult.IsChecked == false &&  CheckBoxSub.IsChecked == false &&   CheckBoxDiv.IsChecked == false)
+                {
+                    OptionsAllCheckBox.IsChecked = false;
+                    MyMathGame.Addition = true;
+                }
+                else
+                {
+                    // Set third state (indeterminate) by setting IsChecked to null.
+                    OptionsAllCheckBox.IsChecked = null;
+                }
+                //Read all Booleans from Window and store in my MathClass object
+                if(CheckBoxAdd.IsChecked.HasValue)
+                    MyMathGame.Addition = CheckBoxAdd.IsChecked.Value;
+                if (CheckBoxDiv.IsChecked.HasValue)
+                    MyMathGame.Division = CheckBoxDiv.IsChecked.Value;
+                if (CheckBoxSub.IsChecked.HasValue)
+                    MyMathGame.Subtraction = CheckBoxSub.IsChecked.Value;
+                if (CheckBoxMult.IsChecked.HasValue)
+                    MyMathGame.Multiplication = CheckBoxMult.IsChecked.Value;
+            }
+        }
+
+        /// <summary>
+        /// Button handler for "Start Game"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Game_Start(object sender, RoutedEventArgs e)
+        {
+            ButtonStart.Content = "Start Game";
+            Questions.Visibility = Visibility.Visible;
+            UserInput.Visibility = Visibility.Visible;
+            ButtonRestartScore.Visibility = Visibility.Visible;
+            TextBlockScore.Visibility = Visibility.Visible;
+            TextBlockGameOver.Visibility = Visibility.Hidden;
+            Questions.Text = MyMathGame.FeedMeTheNextQuestion();
+        }
+
+        private void TextBoxUserInputKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                TextBlockAnswer.Visibility = Visibility.Visible;
+                ProcessAnswer();
+                UserInput.Text = "";
+            }
+        }
+        private void ProcessAnswer()
+        {
+            var ok = Int32.TryParse(UserInput.Text, out int result);
+            if (!ok)
+                return;
+            var IsCorrect = MyMathGame.CheckAnswer(result);
+            TextBlockAnswer.Text = MyMathGame.AnswerSentence;
+            TextBlockScore.Text = $"Your Score is {MyMathGame.TotalScore}";
+            if (IsCorrect)
+                TextBlockAnswer.Background = Brushes.LightGreen;
+            else
+                TextBlockAnswer.Background = Brushes.LightSalmon;
+
+            if (MyMathGame.GameOver())
+            {
+                Questions.Visibility = Visibility.Collapsed;
+                UserInput.Visibility = Visibility.Collapsed;
+                TextBlockAnswer.Visibility = Visibility.Collapsed;
+                TextBlockGameOver.Visibility = Visibility.Visible;
+
+                MyMathGame.EndGame();
+                TextBlockGameOver.Text = MyMathGame.ScoreReport;
+                ButtonStart.Content = "New Game?";
+            }
+            else //Next Question
+            {
+                Questions.Text = MyMathGame.FeedMeTheNextQuestion();
+            }
+        }
+        private void Restart_Score(object sender, RoutedEventArgs e)
+        {
+            MyMathGame.TotalScore = 0;
+            TextBlockScore.Text = $"Your Score is {MyMathGame.TotalScore}";
+        }
+        /// <summary>
+        /// Event handler for slider
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SliderRounds_ValueChanged(object sender, RoutedEventArgs e)
+        {
+
+            var mySlider = sender as Slider;
+            if (mySlider == null)
+                return;
+
+            MyMathGame.GameRounds = (int) mySlider.Value;
+
+        }
+
+        private void Option_Checked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
+
+        private void Option_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
+
+        private void UserInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/MathGame.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfApp1
+{
+    public class MathGame
+    {
+        //public bool asmd = false;
+        public bool Addition = false;
+        public bool Subtraction = false;
+        public bool Multiplication = false;
+        public bool Division = false;
+        public int Score = 0;
+        public int TotalScore = 0;
+        public int QuestionNumber;
+        public string AnswerSentence="";
+        public string ScoreReport="";
+        private Random rnd;
+        private int MaxMDRange; //the maximum value for mulitply and divide
+        private int MaxASRange; //the maximum value for adding and subtration
+        public int GameRounds=5;
+        public string Question="";
+        public int CorrectAnswer;
+        private List<string> ArrayOfPositiveFeedback; //Array
+        //Constructor, called automatically
+        public MathGame()
+        {
+            rnd = new Random();
+            ArrayOfPositiveFeedback = new List<string>
+            {
+                "Correct",
+                "Well Done",
+                "You Rock",
+                "Excellent",
+                "Great Work"
+            };
+        }
+
+        /// <summary>
+        /// Sets the Difficulty Level of the Game
+        /// </summary>
+        /// <param name="Hard"></param>
+        /// <param name="Med"></param>
+        /// <param name="Easy"></param>
+        public void SetDifficultly(bool Hard, bool Med, bool Easy)
+        {
+            if(Hard)
+            {
+                MaxMDRange = 20;
+                MaxASRange = 100;
+            }
+            else if(Med)
+            {
+                MaxMDRange = 15;
+                MaxASRange = 50;
+            }
+            else
+            {
+                MaxMDRange = 10;
+                MaxASRange = 20;
+            }
+
+        }
+
+        public string FeedMeTheNextQuestion()
+        {
+            int Operand = NextQuestionType();
+
+            if (Operand == 1)
+                AdditionGame();
+            else if (Operand == 2)
+                SubtractionGame();
+            else if (Operand == 3)
+                MultiplyGame();
+            else if (Operand == 4)
+                DivideGame();
+            else
+            {
+                //do nothing, error 
+            }
+
+            return Question;
+           
+        }
+
+        /// <summary>
+        /// Determines the next question to ask (1,2,3 or 4) based on 
+        /// 1) random choice And 
+        /// 2) whether the question is allowed
+        /// </summary>
+        /// <returns>Iteger between 1 and 4</returns>
+        public int NextQuestionType()
+        {
+            bool cont = true;
+            int x = 0;
+            while (cont)
+            {
+                x = rnd.Next(1, 5);
+                if (x == 1 && Addition)
+                    cont = false;
+                else if (x == 2 && Subtraction)
+                    cont = false;
+                else if (x == 3 && Multiplication)
+                    cont = false;
+                else if (x == 4 && Division)
+                    cont = false;
+                else
+                {
+                    //The combination was not valid, need to continue in the loop 
+                }
+            }
+            return x;
+        }
+        /// <summary>
+        /// Mulitplication Question, and checks/score answer
+        /// </summary>
+        public void MultiplyGame()
+        {
+            var x = rnd.Next(1, MaxMDRange);
+            var y = rnd.Next(1, MaxMDRange);
+            CorrectAnswer = x * y;
+            Question=$"What is {x} x {y}";
+        }
+        public void DivideGame()
+        {
+            var x = rnd.Next(1, MaxMDRange);
+            var y = rnd.Next(1, MaxMDRange);
+            var z = x * y;
+            CorrectAnswer = x;
+            Question= $"What is {z} / {y}";
+        }
+        public void AdditionGame()
+        {
+            var x = rnd.Next(1, MaxASRange);
+            var y = rnd.Next(1, MaxASRange);
+            CorrectAnswer = x + y;
+            Question= $"What is {x} + {y}";
+        }
+        public void SubtractionGame()
+        {
+            var x = rnd.Next(1, MaxASRange);
+            var y = rnd.Next(1, MaxASRange);
+            var z = x + y;
+            CorrectAnswer = x;
+            Question = $"What is {z} - {y}";
+        }
+        /// <summary>
+        /// Gets user answer, check it with real answer, and score it
+        /// </summary>
+        /// <param name="UserAnswer"></param>
+        public bool CheckAnswer(int UserAnswer)
+        {
+            QuestionNumber++;
+            if (CorrectAnswer == UserAnswer)
+            {
+                Score++;
+                TotalScore++;
+                var FeedbackArrayIndex = rnd.Next(0, ArrayOfPositiveFeedback.Count-1); //Randomly chose the index of the array of positive feedback
+                AnswerSentence = $"{ArrayOfPositiveFeedback[FeedbackArrayIndex]}";
+                return true;
+            }
+            else
+            {
+                AnswerSentence =$"{UserAnswer} is Incorrect\r\nThe answer is {CorrectAnswer}";
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Check for game over
+        /// </summary>
+        /// <returns>True if game over</returns>
+        public bool GameOver()
+        {
+            if (QuestionNumber >= GameRounds)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// Calculates final score, crates report, resets game variables
+        /// </summary>
+        public void EndGame()
+        {
+            double Percentage = ((double)Score / (double)QuestionNumber) * 100;
+            ScoreReport = $"You scored {Score} out of {QuestionNumber}\r\nYour Accuracy is {Percentage:f2}%";
+            Score = 0;
+            QuestionNumber = 0;
+        }
+    }//end of class
+}
